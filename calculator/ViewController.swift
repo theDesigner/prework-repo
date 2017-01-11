1fe547a7a1cba996cab07f547b823193f2cfbb4c
Daniel McGlaughlin <danielmcglaughlin@Daniels-MacBook-Pro.local>
2017-01-11 14:23:24 -0800

    Deleted gradient. Bugged out.

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,28 +8,6 @@
 
 import UIKit
 
-class ViewController: UIViewController  {
-    let gradientLayer = CAGradientLayer()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.green
-        
-        gradientLayer.frame = self.view.bounds
-        
-        let color1 = UIColor.yellow.cgColor as CGColor
-        let color2 = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1.0).cgColor as CGColor
-        let color3 = UIColor.clear.cgColor as CGColor
-        let color4 = UIColor(white: 0.0, alpha: 0.7).cgColor as CGColor
-        gradientLayer.colors = [color1, color2, color3, color4]
-        
-        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
-        
-        self.view.layer.addSublayer(gradientLayer)
-    }
-}
-
-
 class SiewController: UIViewController {
 
     @IBOutlet weak var tipLabel: UILabel!
