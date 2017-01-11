c7b64d0e4d5401d009f0a387576efef401e15d2b
Daniel McGlaughlin <danielmcglaughlin@Daniels-MacBook-Pro.local>
2017-01-11 14:21:40 -0800

    1st Commit. Setting up Git.

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -4,14 +4,42 @@
 //
 //  Created by Daniel McGlaughlin on 1/4/17.
 //  Copyright © 2017 Daniel McGlaughlin. All rights reserved.
-//
+//  Own it!
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {
+    let gradientLayer = CAGradientLayer()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.green
+        
+        gradientLayer.frame = self.view.bounds
+        
+        let color1 = UIColor.yellow.cgColor as CGColor
+        let color2 = UIColor(red: 1.0, green: 0, blue: 0, alpha: 1.0).cgColor as CGColor
+        let color3 = UIColor.clear.cgColor as CGColor
+        let color4 = UIColor(white: 0.0, alpha: 0.7).cgColor as CGColor
+        gradientLayer.colors = [color1, color2, color3, color4]
+        
+        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
+        
+        self.view.layer.addSublayer(gradientLayer)
+    }
+}
+
+
+class SiewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalsLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +48,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func newCalculateTip(_ sender: AnyObject) {
+       
+        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalsLabel.text = String(format: "$%.2f", total)
+
+        
+    }
 
 }
-
