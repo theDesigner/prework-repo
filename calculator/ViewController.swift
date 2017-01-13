bc1cbd8e9539a914fee5a08d8bf3f9792ce1fac8
Daniel McGlaughlin <danielmcglaughlin@daniels-mbp.attlocal.net>
2017-01-12 22:23:12 -0800

    Added animation fade to billTotal label.

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -14,29 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalsLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billTotal: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    }
     @IBAction func onTap(_ sender: AnyObject) {
         view.endEditing(true)
-    }
+        self.billField.alpha = 0
+        self.billTotal.alpha = 1
+        UIView.animate(withDuration: 0.4, animations: {
+            // This causes first view to fade in and second view to fade out
+            self.billField.alpha = 1
+            self.billTotal.alpha = 0
+        })
     
+    }
     @IBAction func newCalculateTip(_ sender: AnyObject) {
        
         let tipPercentages = [0.15, 0.18, 0.20]
-        
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
+        
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalsLabel.text = String(format: "$%.2f", total)
